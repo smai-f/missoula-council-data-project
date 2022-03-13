0e07565c52f6b78ade7822956c329291badddc09
Smai Fullerton <shfullerton@gmail.com>
2022-03-13 17:33:14 -0600

    Scraper implementation sketch

--- python/cdp_missoula_backend/scraper.py
+++ python/cdp_missoula_backend/scraper.py
@@ -36,5 +36,21 @@ def get_events(
     from GitHub Actions UI.
     """
 
-    # Your implementation here
+    # Go to https://pub-missoula.escribemeetings.com/?Year=2022
+    # within div class=past-meetings -> calendar-item
+
+    ## COMMITTEE NAME
+
+    # meeting-title -> <a> child text ("City Council Meeting")
+
+    ## MEETING DATE
+
+    # meeting-date child text ("Monday, 14 March 2022 @ 6:00 PM")
+
+    ## VIDEO FILE
+
+    # Click on every single <a href=./Players/ISIStandAlonePlayer.aspx?*
+    # On new page, grab the div id=isi_player's data-file_name (Encoder1_AF_2022-03-02-02-03.mp4)
+    # The video link is https://video.isilive.ca/missoula/ + value from previous step
+
     return []
