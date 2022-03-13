e78d28f56ba0c128997ae9b83e215abc50e3aec6
Smai Fullerton <shfullerton@gmail.com>
2022-03-13 16:18:31 -0600

    First post

---
+++ python/cdp_missoula_backend/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Top-level package for Missoula
+CDP Instance backend.
+"""
+
+__author__ = "Smai Fullerton"
+__version__ = "1.0.0"
+
+
+def get_module_version() -> str:
+    return __version__
