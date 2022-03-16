d7cc54cd2705e5e0c86e5df0e7908fc585622514
Smai Fullerton <shfullerton@gmail.com>
2022-03-15 22:36:25 -0600

    Hmm

--- python/cdp_missoula_backend/scraper.py
+++ python/cdp_missoula_backend/scraper.py
@@ -38,30 +38,32 @@ def get_events(
     # Go to https://pub-missoula.escribemeetings.com/?Year=2022
     # within div class=past-meetings -> calendar-item
 
-    ## COMMITTEE NAME
+    # COMMITTEE NAME
 
     # meeting-title -> <a> child text ("City Council Meeting")
 
-    ## MEETING DATE
+    # MEETING DATE
 
     # meeting-date child text ("Monday, 14 March 2022 @ 6:00 PM")
 
-    ## VIDEO FILE
+    # VIDEO FILE
 
     # Click on every single <a href=./Players/ISIStandAlonePlayer.aspx?*
-    # On new page, grab the div id=isi_player's data-file_name (Encoder1_AF_2022-03-02-02-03.mp4)
+    # On new page, grab the div id=isi_player's data-file_name 
+    # (Encoder1_AF_2022-03-02-02-03.mp4)
     # The video link is https://video.isilive.ca/missoula/ + value from previous step
 
-
     hardcoded_mtg = ingestion_models.EventIngestionModel(
-        body=ingestion_models.Body(name="Affordable Housing Resident Oversight Committee"),
-        sessions=[
-            ingestion_models.Session(
-                video_uri="https://video.isilive.ca/missoula/Encoder1_AHROC_2022-03-09-07-51.mp4",
-                session_datetime=datetime(2022, 3, 9, 18),
-                session_index=0,
-            ),
-        ],
+      body=ingestion_models.Body(
+        name="Affordable Housing Resident Oversight Committee"
+      ),
+      sessions=[
+        ingestion_models.Session(
+          video_uri="https://video.isilive.ca/missoula/Encoder1_AHROC_2022-03-09-07-51.mp4",
+          session_datetime=datetime(2022, 3, 9, 18),
+          session_index=0,
+        ),
+      ],
     )
 
     return [hardcoded_mtg]
